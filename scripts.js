37784cc905b016ff372434b89ad511fe43fdd6cf
Alla P <evetrueivory@gmail.com>
2017-03-13 17:46:58 +0300

    добавила стрелку «далее» справа

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
 
+function getArrow() {
+  document.getElementsByClassName("rightside-box")[0].style.backgroundColor = "blue";
+  document.getElementsByClassName("next-arrow")[0].style.visibility = "visible";
+
+}
+
+function eraseArrow() {
+  document.getElementsByClassName("rightside-box")[0].style.backgroundColor = "white";
+  document.getElementsByClassName("next-arrow")[0].style.visibility = "hidden";
+
+}
+
+function getNextItem() {
+  document.getElementsByClassName("current-photo")[0].src = "";
+}

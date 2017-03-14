56c0f36c2e515dedb089671d9a3da703563b0a07
Alla P <evetrueivory@gmail.com>
2017-03-14 20:24:58 +0300

    отредактирована разметка, сделано меню слева, добавлено первое описание собаки

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 
 function getArrow() {
+  document.getElementsByClassName("rightside-box")[0].style.display = "flex";
   document.getElementsByClassName("rightside-box")[0].style.backgroundColor = "blue";
   document.getElementsByClassName("next-arrow")[0].style.visibility = "visible";
-
 }
 
 function eraseArrow() {
@@ -13,4 +13,19 @@ function eraseArrow() {
 
 function getNextItem() {
   document.getElementsByClassName("current-photo")[0].src = "";
+}
+
+// показываем описание к Doge
+
+function showDogeInfo() {
+  document.getElementsByClassName("photo-discription")[0].style.display = "flex";
+  document.getElementsByClassName("photo-discription")[0].getElementsByClassName("doge-disc")[0].style.display = "flex";
+  document.getElementsByClassName("first-part")[0].style.display = "flex";
+
+}
+
+function revealMoreDoge() {
+  document.getElementsByClassName("show-more")[0].style.display = "none";
+  document.getElementsByClassName("second-part")[0].style.display = "flex";
+  document.getElementsByClassName("photo-discription")[0].style.height = "240px";
 }

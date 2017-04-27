9fc14d967796d59ee8a9e42b5e21d03dfcde1830
Alla P <evetrueivory@gmail.com>
2017-04-27 14:26:03 +0300

    Переписаны классы элементов, упорядочены стили в соответствии с классами. Подключила загрузку фотографий категории с сервера. Уточнены названия функций. Добавила loader’ы при загрузке страницы и во время загрузки новой фотографии категории по клику. Временно скрыто описание с фото.

--- scripts.js
+++ scripts.js
@@ -1,160 +1,96 @@
-// описания
 
 
-var textDoge = "Doge is an Internet meme that became popular in 2013. The meme typically consists of a picture " +
-    "of a Shiba Inu accompanied by multicolored text in Comic Sans font in the foreground. The text, representing a " +
-    "kind of internal monologue, is deliberately written in a form of broken English. " +
-    "The meme is based on a 2010 photograph, and became popular in late 2013, being named as Know Your Meme's \"top meme\" " +
-    "of that year. A cryptocurrency based on Doge, the Dogecoin, was launched in December 2013, and the Shiba Inu has " +
-    "been featured on Josh Wise's NASCAR car as part of a sponsorship deal. Doge has also been referenced by members " +
-    "of the United States Congress, a safety video for Delta Air Lines, a Google Easter egg, and the video for the song " +
-    "\"Word Crimes\" by \"Weird Al\" Yankovic.";
-
-var textHusky = "Husky is a general name for a sled-type of dog used in northern regions, differentiated " +
-    "from other sled-dog types bytheir fast pulling style. They are an ever-changing cross-breed of the fastest dogs. " +
-    "The Alaskan Malamute, by contrast, is \"the largest and most powerful\" sled dog, and was used for heavier loads. " +
-    " Huskies are used in sled dog racing. In recent years, companies have been marketing tourist treks with dog sledges " +
-    "for adventure travelers in snow regions as well. Huskies are also today kept as pets, and groups work to find new pet " +
-    "homes for retired racing and adventure trekking dogs." +
-    "Huskies are energetic and athletic. They usually have a thick double coat that can be gray, black, copper red, or white." +
-    "Their eyes are typically pale blue, although they may also be brown, green, blue, yellow, or heterochromic. Huskies " +
-    "are more prone to some degree of uveitis than most other breeds.";
-
-var textDoggest = "";
-var textShiber = "";
-var textPuggerino = "";
-var textPupper = "";
-var abourShoobi = "";
-
-var textArray = [textDoge, textHusky, textDoggest, textShiber, textPuggerino, textPupper, abourShoobi];
-
-
-var dFP = "images/doge-full-image.jpg"; // первая пикча доге
-var dSP = "images/doge-2.jpg"; //вторая пикча доге
-var dTP = "images/doge-3.jpg"; //третья пикча доге
-
-var hFP = "images/husky-1.jpg";
-var hSP = "images/husky-2.jpg";
-var hTP = "images/husky-3.jpg";
-
-var dogeImageArr = [dFP, dSP, dTP];
-var huskyImageArr = [hFP, hSP, hTP];
-
-// var dogsMap = {
-//     "doge": {
-//         "id": "doge",
-//         "name": "Doge",
-//         "picture": dogeImageArr,
-//         "description": textDoge,
-//         "count": 0
-//     },
-//     "husky": {
-//         "id": "husky",
-//         "name": "Husky",
-//         "picture": huskyImageArr,
-//         "description": textHusky,
-//         "count": 0
-//     },
-//     "doggest": {
-//         "id": "doggest",
-//         "name": "Doggest",
-//         "picture": " ",
-//         "description": " "
-//     },
-//     "shiber": {
-//         "id": "shiber",
-//         "name": "Shiber",
-//         "picture": " ",
-//         "description": " "
-//     },
-//     "puggerino": {
-//         "id": "puggerino",
-//         "name": "Puggerino",
-//         "picture": " ",
-//         "description": " "
-//     },
-//     "pupper": {
-//         "id": "pupper",
-//         "name": "Pupper",
-//         "picture": " ",
-//         "description": " "
-//     },
-//     "shoobi": {
-//         "id": "shoobi",
-//         "name": "Shoobi",
-//         "picture": " ",
-//         "description": " "
-//     }
-// };
+var currentDogIndex;
+var currentDogId;
 
 var dogs_array;
+var dogs_photos;
 
+var countMap = {
+    "doge": 0,
+    "husky": 0
+};
 
-var currentDog;
-
-var currentPhoto;
-
-
-
-// adding new objects to database
-// function writeDogData(newId, newName, newPicture, newDescription, newCount) {
-//     firebase.database().ref('dogs/' + newId).set({
-//         id: newId,
-//         name: newName,
-//         picture: newPicture,
-//         description: newDescription,
-//         count: newCount
-//     });
-// }
-// writeDogData("husky", "Husky", hFP, textHusky, 0);
-
-
-// var dogsRef = firebase.database().ref('dogs/'); // get info from db
-// dogsRef.on('value', snapshot => {
-//   preObject.innerHTML = JSON.stringify(snapshot.val());
-// });
-
-// var userId = firebase.database().ref("dogs/" + curType);
-// такс такс что это тут ооооо js кул код бэйба заходи в гости: вставлю тебе ПАРУ ТЭГОВ )))
+window.onload = main;
 
 
 
+function main() {
 
+    console.log('main');
+    firebase.database().ref("dogs_array").on('value', onDogsLoaded, onNetworkError);
+}
+
+
+function onDogsLoaded(snapshot) {
+    console.log('onDogsLoaded');
+    dogs_array = snapshot.val();
+    listDoggos();
+
+    openSelectedDog(0);
+}
+
+// загрузить список собак
 function listDoggos() {
-
     var list = document.getElementById("leftside-list");
     for (var i = 0; i < dogs_array.length; i++) {
         list.insertAdjacentHTML("beforeEnd", "<div></div>");
         var listItem = list.children[i];
         listItem.id = dogs_array[i].id;
         listItem.innerHTML = dogs_array[i].name;
+        listItem.className += "leftside-menu__item";
         var url = dogs_array[i].picture_url;
         listItem.style.backgroundImage = "url('" + url + "')";
 
-        addListenerToView(listItem, dogs_array[i]);
+        addListenerToView(listItem, i);
     }
+
+    console.log("list was created");
+    console.log("add default photo");
+
 }
 
-function addListenerToView(childViewParam, dogCategoryParam) { // добавляем onclick
-    childViewParam.addEventListener('click', () => {
-        showFirstInfo(dogCategoryParam);
+// добавить событие по клику
+function addListenerToView(childView, i) { // добавить onclick
+    childView.addEventListener('click', () => {
+        openSelectedDog(i);
     });
 }
 
-function changeHeight() {
-    document.getElementById("photo-description").classList.toggle("extended");
-}
-// при нажатии на кнопку
-function showFirstInfo(key) {
-
-    currentPhoto.src = dogsMap[key]["picture"][dogsMap[currentDog]["count"]]; // показать фото
-    document.getElementById("photo-description").style.display = "flex"; //  показать блок с описанием
-    document.getElementById("text").innerHTML = dogsMap[key]["description"]; // загрузить текст
+// выбрать категорию
+function openSelectedDog(i) {
+    currentDogIndex = i;
+    currentDogId = dogs_array[i].id;
+    firebase.database().ref("dogs_photos/" + currentDogId).on('value', onPhotosLoaded, onNetworkError);
 }
 
-// открывать и закрывать текст
-function showInfo() {
-    if (document.getElementById("photo-description").className != "extended") { // если текст свернут
+
+function onPhotosLoaded(snapshot) {
+    console.log('onPhotosLoaded');
+    dogs_photos = snapshot.val();
+    var currentDogCount = countMap[currentDogId];
+
+    showPhoto(dogs_photos[currentDogCount]);
+    showDesc(dogs_array[currentDogIndex].description);
+
+    hidePageLoader();
+}
+
+// загрузить фото
+function showPhoto(photo) {
+    document.getElementById("current-photo").src = photo; // показать фото
+
+}
+
+// загрузить описание
+function showDesc(desc) {
+    document.getElementById("photo-description-id").classList.remove("photo-description_hidden"); // показать блок с описанием
+    document.getElementById("text").innerHTML = desc; // загрузить текст
+}
+
+// свернуть/развернуть описание; поменять текст кнопки
+function revealText() {
+    if (document.getElementById("photo-description-id").className != "photo-description extended") { // если текст свернут
         document.getElementById("reveal-button").innerHTML = "Show less..."; // поменять текст на кнопке
     } else {
         document.getElementById("reveal-button").innerHTML = "Show more..."; // поменять текст кнопки
@@ -162,40 +98,35 @@ function showInfo() {
     changeHeight();
 }
 
+// поменять высоту блока описания
+function changeHeight() {
+    document.getElementById("photo-description-id").classList.toggle("extended");
+}
+
+// открыть следующее фото по клику
 function getNextPic() {
-    dogsMap[currentDog]["count"]++;
-    if (dogsMap[currentDog]["count"] >= dogsMap[currentDog]["picture"].length) {
-        dogsMap[currentDog]["count"]--;
-    }
-    document.getElementById("current-photo").src = dogsMap[currentDog]["picture"][dogsMap[currentDog]["count"]];
-    // alert("This dog count: " + dogsMap[currentDog]["count"]);
+
+    displayPhotoLoader();
+    document.getElementById("current-photo").src = "";
+
+    var currentDogCount = countMap[currentDogId];
+    currentDogCount = (currentDogCount + 1) % dogs_photos.length;
+    countMap[currentDogId] = currentDogCount;
+
+    document.getElementById("current-photo").src = dogs_photos[currentDogCount];
+}
+
+// показать/скрыть загрузчик
+function hidePageLoader() {
+        document.getElementById("page-loader").classList.add("loader_hidden");
+}
+
+function displayPhotoLoader() {
+    document.getElementById("photo-loader").classList.remove("loader_hidden");
+
 }
 
 
-
-
-
-
-
-function main() {
-    console.log('main')
-    firebase.database().ref("dogs_array").once('value', onDogsLoaded, onNetworkError);
-}
-
-
-function onDogsLoaded (snapshot) {
-    console.log('onDogsLoaded');
-
-    dogs_array = snapshot.val();
-    listDoggos();
-}
-
-function onNetworkError (error) {
-    console.log('onNetworkError');
+function onNetworkError(error) {
     console.log(error);
 }
-
-
-
-window.onload = main;
-
